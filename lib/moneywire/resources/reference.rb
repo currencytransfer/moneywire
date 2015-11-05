bfa75f0308a9419dd502b5b5a6a8f66a470515cf
Kamen Hursev <kamen@currencytransfer.com>
2015-11-05 18:18:29 +0200

    Integrate the reference endpoints

--- lib/moneywire/resources/reference.rb
+++ lib/moneywire/resources/reference.rb
@@ -3,8 +3,20 @@ module Moneywire
     class Reference < BaseResource
       use_resource 'reference'
 
+      def available_currencies
+        get('available-currencies')
+      end
+
+      def balances
+        get('balances')
+      end
+
       def settlement_accounts(currency)
         get("settlement-accounts/#{currency}")
+      end
+
+      def invalid_conversion_dates(currency_pair)
+        get("invalid-conversion-dates/#{currency_pair}")
       end
     end
   end
--- spec/integration/reference_spec.rb
+++ spec/integration/reference_spec.rb
@@ -5,8 +5,35 @@ describe 'Integration with reference', vcr: true do
     Moneywire::Client.new(SpecConfig.login_id, SpecConfig.api_key, SpecConfig.token)
   end
 
+  describe 'available_currencies' do
+    it 'returns arrays with sell and buy currencies' do
+      expect(client.reference.available_currencies).to(
+        include('sellCurrencies' => be_an(Array), 'buyCurrencies' => be_an(Array))
+      )
+    end
+  end
+
+  describe 'balances' do
+    let(:result) { client.reference.balances }
+    let(:expected_fields) do
+      %w(
+        currency
+        settledFunds
+        deposits
+        payments
+        deductions
+        availableToWithdraw
+        transactionLimit
+      )
+    end
+
+    it 'returns current balance information' do
+      expect(client.reference.balances['balances']).to all include(*expected_fields)
+    end
+  end
+
   describe 'settlement_accounts' do
-    let(:expected_response_fields) do
+    let(:expected_fields) do
       %w(
         accountType
         currency
@@ -18,7 +45,19 @@ describe 'Integration with reference', vcr: true do
       )
     end
     it 'returns settlement account information' do
-      expect(client.reference.settlement_accounts('USD')).to include(*expected_response_fields)
+      expect(client.reference.settlement_accounts('USD')).to include(*expected_fields)
+    end
+  end
+
+  describe 'invalid_conversion_dates' do
+    it 'returns information for invalid conversion dates' do
+      expect(client.reference.invalid_conversion_dates('GBPUSD')).to(
+        include(
+          'invalid_conversion_dates',
+          'first_conversion_date',
+          'default_conversion_date'
+        )
+      )
     end
   end
 end
