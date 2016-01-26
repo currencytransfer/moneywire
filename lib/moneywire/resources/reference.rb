c626cd370f9d7f8db3ad0111e9bddcd042ebe009
Kamen Hursev <kamen@currencytransfer.com>
2016-01-26 18:23:29 +0200

    Update reference API calls

--- lib/moneywire/resources/reference.rb
+++ lib/moneywire/resources/reference.rb
@@ -19,8 +19,8 @@ module Moneywire
         get("settlement-accounts/#{conversion_id}")
       end
 
-      def invalid_conversion_dates(currency_pair)
-        get("invalid-conversion-dates/#{currency_pair}")
+      def conversion_dates(**params)
+        get('conversion-dates', params)
       end
     end
   end
