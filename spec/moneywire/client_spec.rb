3071dd65974a4fdecac9177fbef53dc77d5fddf1
Kamen Hursev <kamen@currencytransfer.com>
2016-06-28 16:45:57 +0300

    CT-1544: [SPEC] Check if acting_for is properly passed to Resourceis classes

--- spec/moneywire/client_spec.rb
+++ spec/moneywire/client_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
 
 describe Moneywire::Client do
+  let(:client) do
+    described_class.new(
+      'login_id', 'api_key',
+      token: 'token', totp_token: 'totp', acting_for: 1
+    )
+  end
+
   describe '.new' do
     it 'creates a RequestHandler instance' do
       expect(Moneywire::RequestHandler).to receive(:new).with('login_id', 'api_key', 'token', nil)
-      Moneywire::Client.new(
+      described_class.new(
         'login_id', 'api_key',
         token: 'token', totp_token: 'totp', acting_for: 1
       )
@@ -19,7 +26,7 @@ describe Moneywire::Client do
 
       it 'requests authentication' do
         expect(request_handler).to receive(:authenticate)
-        Moneywire::Client.new(
+        described_class.new(
           'login_id', 'api_key', totp_token: 'totp', acting_for: 1
         )
       end
@@ -34,11 +41,44 @@ describe Moneywire::Client do
 
       it 'does not request authentication' do
         expect(request_handler).not_to receive(:authenticate)
-        Moneywire::Client.new(
+        described_class.new(
           'login_id', 'api_key',
           token: 'token', totp_token: 'totp', acting_for: 1
         )
       end
+    end
+  end
+
+  describe '#quotes' do
+    it 'creates a resource instance passing acting_for parameter' do
+      expect(Moneywire::Resources::Quotes).to receive(:new).with(
+        client.request_handler,
+        acting_for: 1
+      )
+
+      client.quotes
+    end
+  end
+
+  describe '#conversions' do
+    it 'creates a resource instance passing acting_for parameter' do
+      expect(Moneywire::Resources::Conversions).to receive(:new).with(
+        client.request_handler,
+        acting_for: 1
+      )
+
+      client.conversions
+    end
+  end
+
+  describe '#reference' do
+    it 'creates a resource instance passing acting_for parameter' do
+      expect(Moneywire::Resources::Reference).to receive(:new).with(
+        client.request_handler,
+        acting_for: 1
+      )
+
+      client.reference
     end
   end
 end
