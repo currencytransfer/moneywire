d129700a13cefbd64900db1498bd63c6db84b75e
Kamen Hursev <kamen@currencytransfer.com>
2017-09-25 15:09:53 +0300

    Change production URLs

--- lib/moneywire.rb
+++ lib/moneywire.rb
@@ -3,12 +3,12 @@ require 'moneywire/client'
 
 module Moneywire
   ENVIRONMENTS = {
-    production: 'https://public-api.supercapital.uk/v1/',
+    production: 'https://currencytransfer-api.supercapital.uk/v1/',
     demo: 'https://public-demo-api.supercapital.uk/v1/'
   }.freeze
 
   FILE_UPLOADS = {
-    production: 'https://file.supercapital.uk/v1/',
+    production: 'https://currencytransfer-file-api.supercapital.uk/v1/',
     demo: 'https://file-demo.supercapital.uk/v1/'
   }.freeze
 
